1fe68a08402bc3663a346fa7e88cbf2f6792255b
Thong Le <thongle24012003@gmail.com>
2025-12-18 00:27:17 +0700

    thonglh2: search food

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -174,6 +174,7 @@ export default function Index() {
 
         if (searchKeyword) {
           params.append("nameSearch", searchKeyword);
+          params.append("categoryFoodSearch", searchKeyword);
         }
 
         if (selectedCategory) {
